40f302ee2eff72191b06b9f1b945421fdb6258f8
Stiven Dias <diasstiven2@gmail.com>
2026-02-23 13:10:11 -0100

    teste final

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express"
+
+
+const app = express(); // cria a aplicação
+
+// rota inicial
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+// inicia o servidor na porta 3000
+app.listen(3000, () => {
+  console.log("Servidor rodando em http://localhost:3000");
+});
+ 
