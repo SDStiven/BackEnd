483be9845a1e951385b35e25b89d29c9c9c341f6
Stiven Dias <diasstiven2@gmail.com>
2026-03-11 20:58:00 -0100

    tic-tac-toe

--- Learning/TicTacToe/src/Board.jsx
+++ Learning/TicTacToe/src/Board.jsx
@@ -84,18 +84,19 @@ function calculateWinner(squares) {
 
 // função que renderiza o jogo  
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
-
-  function handlePlay(nextSquares) {
-    // TODO
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    const xIsNext = currentMove % 2 === 0;
+    
+    function handlePlay(nextSquares) {
+      // TODO
+      setHistory([...history, nextSquares]);
+      setXIsNext(!xIsNext);
+      const currentSquares = history[history.length - 1];
   }
 
   function jumpTo(nextMove) {
+      setCurrentMove(nextMove);
     // TODO
   }
 
